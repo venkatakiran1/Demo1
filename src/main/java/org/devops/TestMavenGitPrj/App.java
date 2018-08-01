0acb0b78f86536464d9c9e3232faf326dffa5831
venkata Kiran <bhargavi.jaya@gmail.com>
2018-08-01 23:32:50 +0530

    Initial commit

---
+++ src/main/java/org/devops/TestMavenGitPrj/App.java
@@ -0,0 +1,13 @@
+package org.devops.TestMavenGitPrj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is from maven workspace" );
+    }
+}
