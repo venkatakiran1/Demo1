3c38d2abbb56b0578235293395da63fc34c96904
venkata Kiran <bhargavi.jaya@gmail.com>
2018-08-01 23:38:57 +0530

    one more commit from console

--- src/main/java/org/devops/TestMavenGitPrj/App.java
+++ src/main/java/org/devops/TestMavenGitPrj/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello This is from maven workspace" );
+        System.out.println( "Hello This is from maven workspace!! adding one line" );
     }
 }
